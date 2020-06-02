78f79e9b7d7f3adb9760c35ff5fb53cedabf0cad
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2020-06-02 21:19:01 +0400

    Add files via upload

---
+++ ClassLibrary/ClassLibrary/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    //dll-dinamic link library
+    class Main
+    {
+        public Main()
+        {
+            Notification not = new Notification();
+            not.MyProperty = 15;
+        }
+    }
+    public class Notification
+    {
+        protected internal int MyProperty { get; set; }
+        private protected string Title;
+    }
+
+    class Sms : Notification 
+    {
+        public void Test()
+        {
+            MyProperty = 15;
+            Title = "Information";
+        }
+    }
+}
